0875dcc3da461246a7f3daed0bc90255632fe541
dojo2023-nakamura <nakamuramaikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-15 17:12:04 +0900

    Update paymentServlet.java

--- src/src/servlet/paymentServlet.java
+++ src/src/servlet/paymentServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class paymentServlet
@@ -22,6 +23,10 @@ public class paymentServlet extends HttpServlet {
 	  protected void doGet(HttpServletRequest request,
 		      HttpServletResponse response)
 		      throws ServletException, IOException {
+
+			//セッションスコープにあるuseridを取得
+			HttpSession session = request.getSession();
+			String userid = (String)session.getAttribute("userid");
 
 		    // フォワード
 		    RequestDispatcher dispatcher =
