4e1e37834fdadb04c66d503dc7b5ac63e7583337
dojo2023-iijima <130334564+dojo2023-iijima@users.noreply.github.com>
2023-06-16 15:40:56 +0900

    Update registerServlet.java

--- src/src/servlet/registerServlet.java
+++ src/src/servlet/registerServlet.java
@@ -9,9 +9,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import dao.IdpwDAO;
-import model.Idpw;
+import dao.idpwsDAO;
+import dao.paymentsDAO;
+import dao.buyttersDAO;
+import dao.goalsDAO;
+import dao.charactersDAO;
+import dao.charpicsDAO;
+import dao.nicebuycountsDAO;
+import dao.pointsDAO;
+import dao.historysDAO;
+import dao.itemspicsDAO;
+import dao.itemsDAO;
+import dao.banksDAO;
+import model.Idpws;
+import model.Payments;
 import model.Result;
+import model.Buytters;
+import model.Calendar;
+import model.CalendarDate;
+import model.Room;
+import model.achievement;
+import model.Ranking;
+import model.ResultGoals;
+import model.Character;
+import model.PictuerBook;
+import model.Result;
+import model.DressUp;
 
 /**
  * Servlet implementation class registerServlet
@@ -43,7 +66,7 @@ public class registerServlet extends HttpServlet {
 				String userid = request.getParameter("userid");
 				String userpw = request.getParameter("userpw");
 				String username = request.getParameter("username");
-				
+
 				IdpwDAO iDao = new IdpwDAO();
 
 				if (iDao.register(new Idpw(id,pw)) == 0) {	// 登録成功
