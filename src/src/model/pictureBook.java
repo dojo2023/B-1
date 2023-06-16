fafd5df36b4dcb5c0350e1d78e0e2ada134100d2
dojo2023-Hanawa <hanawaakihitoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-16 15:30:35 +0900

    Create pictureBook.java

---
+++ src/src/model/pictureBook.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.io.Serializable;
+
+public class pictureBook implements Serializable{
+	private String charpic;
+	private int charid;
+	public pictureBook(String charpic, int charid) {
+		super();
+		this.charpic = charpic;
+		this.charid = charid;
+	}
+	public String getCharpic() {
+		return charpic;
+	}
+	public void setCharpic(String charpic) {
+		this.charpic = charpic;
+	}
+	public int getCharid() {
+		return charid;
+	}
+	public void setCharid(int charid) {
+		this.charid = charid;
+	}
+
+
+}
