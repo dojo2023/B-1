a3c42f4abf07c9c7def45aa6727c8215399b3263
dojo2023-ikadat <130334525+dojo2023-ikadat@users.noreply.github.com>
2023-06-18 03:44:29 +0900

    Update Buytters.java

--- src/src/model/Buytters.java
+++ src/src/model/Buytters.java
@@ -4,33 +4,43 @@ import java.io.Serializable;
 
 public class Buytters implements Serializable{
 
-	private String userid;
-	private String bcommnet;
+	private String id;
+	private String user_id;
+	private String b_comment;
 	private String b_pic;
-	private String createdat;//もしかしたら、Date型かもわからん
+	private String created_at;//もしかしたら、Date型かもわからん
 
-	public Buytters(String userid, String bcommnet, String b_pic, String createdat) {
+	public Buytters(String id, String user_id, String b_comment, String b_pic, String created_at) {
 		super();
-		this.userid = userid;
-		this.bcommnet = bcommnet;
+		this.id = id;
+		this.user_id = user_id;
+		this.b_comment = b_comment;
 		this.b_pic = b_pic;
-		this.createdat = createdat;
+		this.created_at = created_at;
 	}
 
-	public String getUserid() {
-		return userid;
+	public String getId() {
+		return id;
 	}
 
-	public void setUserid(String userid) {
-		this.userid = userid;
+	public void setId(String id) {
+		this.id = id;
 	}
 
-	public String getBcommnet() {
-		return bcommnet;
+	public String getUser_id() {
+		return user_id;
 	}
 
-	public void setBcommnet(String bcommnet) {
-		this.bcommnet = bcommnet;
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getB_comment() {
+		return b_comment;
+	}
+
+	public void setB_comment(String b_comment) {
+		this.b_comment = b_comment;
 	}
 
 	public String getB_pic() {
@@ -41,12 +51,12 @@ public class Buytters implements Serializable{
 		this.b_pic = b_pic;
 	}
 
-	public String getCreatedat() {
-		return createdat;
+	public String getCreated_at() {
+		return created_at;
 	}
 
-	public void setCreatedat(String createdat) {
-		this.createdat = createdat;
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
 	}
 
 }
