0fe3a669bd5caa537f69055acb2ebe8e53d103e0
dojo2023-ikadat <130334525+dojo2023-ikadat@users.noreply.github.com>
2023-06-18 03:44:22 +0900

    Update buytterServlet.java

--- src/src/servlet/buytterServlet.java
+++ src/src/servlet/buytterServlet.java
@@ -1,67 +1,82 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import dao.idpwsDAO;
-import dao.paymentsDAO;
-import dao.buyttersDAO;
-import dao.goalsDAO;
-import dao.charactersDAO;
-import dao.charpicsDAO;
-import dao.nicebuycountsDAO;
-import dao.pointsDAO;
-import dao.historysDAO;
-import dao.itemspicsDAO;
-import dao.itemsDAO;
-import dao.banksDAO;
-import model.Idpws;
-import model.Payments;
-import model.Result;
+
+import dao.buytterDAO;
 import model.Buytters;
-import model.LoginUser;
-import model.Calendar;
-import model.CalendarDate;
-import model.Room;
-import model.Achievement;
-import model.Ranking;
-import model.ResultGoals;
-import model.Character;
-import model.PictureBook;
-import model.Result;
-import model.DressUp;
-/**
- * Servlet implementation class buytterServlet
- */
 @WebServlet("/buytterServlet")
 public class buytterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+
+// まずはGetリクエストの処理
+// (「URLの入力」がされた時 or「この画面に遷移」の入力がされた時)
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	  protected void doGet(HttpServletRequest request,
-		      HttpServletResponse response)
-		      throws ServletException, IOException {
+	  protected void doGet(HttpServletRequest request,HttpServletResponse response)
+			  throws ServletException, IOException {
 
-		    // フォワード
-		    RequestDispatcher dispatcher =
+//		// もしもログインしていなかったらloginServlet.javaにリダイレクトする
+//			HttpSession session = request.getSession();
+//			if (session.getAttribute("userid") == null) {
+//				response.sendRedirect("/Ifrit/loginServlet");
+//				return;
+//			}
+
+		  // ログイン情報があれば、最終的にbuytter.jspにフォワードしたい
+		  // top画面はTL画面で、最新buyeet順に表示
+
+		  // 配列宣言
+		  ArrayList<Buytters> buyeetList = new ArrayList<Buytters>();
+
+		  // buytterDAOのオブジェクト宣言
+		  buytterDAO objDao = new buytterDAO();
+		  buyeetList = objDao.selectAll();
+		  // 検索結果をリクエストスコープに格納する
+		  request.setAttribute("buyeetList", buyeetList);
+
+		// buytter.jspにフォワードするよ
+//		    RequestDispatcher dispatcher =
 		        request.getRequestDispatcher
-		            ("/WEB-INF/jsp/buytter.jsp");
-		    dispatcher.forward(request, response);
+		            ("/WEB-INF/jsp/buytter.jsp").forward(request, response);
 		  }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
+// 次はPOSTリクエストの処理
+// nice buyボタン押された時、バイートボタン押された時、検索ボタン押された時
+
+	  	/**
+		 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+		 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+//		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+//			HttpSession session = request.getSession();
+//			if (session.getAttribute("userid") == null) {
+//				response.sendRedirect("/Ifrit/LoginServlet");
+//				return;
+//			}
+
+		// まずはnice buyボタン押された時の処理
+
+
+
+		// バイートボタン押された時の処理
+
+
+
+		// 最後は検索ボタン押された時の処理
+
+
+
+	}
 }
