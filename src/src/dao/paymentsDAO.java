24ea6c308f18f8a5e862a1505eae66bd63b6db09
dojo2023-iijima <130334564+dojo2023-iijima@users.noreply.github.com>
2023-06-20 13:38:23 +0900

    Update paymentsDAO.java

--- src/src/dao/paymentsDAO.java
+++ src/src/dao/paymentsDAO.java
@@ -66,4 +66,55 @@ public List<Payments> select(Payments Payments) {
 	// 結果を返す
 	return paymentsList;
 }
+
+// achievementServletの支出合計取得のselect
+public int select(String userid) {
+	Connection conn = null;
+
+	int sisyutsu;
+
+	try {
+		// JDBCドライバを読み込む
+		Class.forName("org.h2.Driver");
+
+		// データベースに接続する
+		conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/B1", "sa", "");
+
+		// SQL文を準備する
+		String sql = "select sum (pay_money) from payments where user_id=?";
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		// SQL文を完成させる(servletから取得したuserid(11.のString userid)をSQLに入れる)
+		pStmt.setString(1, userid);
+
+		// SQL文を実行し、結果表を取得する
+		ResultSet rs = pStmt.executeQuery();
+
+		// 結果表をコレクションにコピーする
+		sisyutsu = rs.getInt("pay_money");
+	}
+	catch (SQLException e) {
+		e.printStackTrace();
+		sisyutsu = 0;
+	}
+	catch (ClassNotFoundException e) {
+		e.printStackTrace();
+		sisyutsu = 0;
+	}
+	finally {
+		// データベースを切断
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+				sisyutsu = 0;
+			}
+		}
+	}
+
+// 結果を返す
+return sisyutsu;
+}
 }
