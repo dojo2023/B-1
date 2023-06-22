a625100dc368435dff40d2c96dd96762d5f38818
dojo2023-togashi <130334622+dojo2023-togashi@users.noreply.github.com>
2023-06-22 14:42:12 +0900

    Update calendarServlet.java

--- src/src/servlet/calendarServlet.java
+++ src/src/servlet/calendarServlet.java
@@ -9,6 +9,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import dao.banksDAO;
+import dao.charpicsDAO;
+import dao.goalsDAO;
+import model.CalendarDate;
 /**
  * Servlet implementation class calendarServlet
  */
@@ -30,9 +35,35 @@ public class calendarServlet extends HttpServlet {
 				return;
 		}
 
+			String userid = (String)session.getAttribute("userid");
+	    // 欲しいもの名を出す
+			goalsDAO gd = new goalsDAO ();
+			String gwant = gd.getwant(userid);
+			System.out.println(gwant);
+
+		// 残り日数(glimit - 今日)
+			goalsDAO gdao = new goalsDAO();
+			int glimit = gdao.calendarlimit(userid);
+			System.out.println(glimit);
+
+		// キャラ背景画像
+			charpicsDAO cdao = new charpicsDAO();
+			String charpic = cdao.charpic(userid);
+			System.out.println(charpic);
+
+		// 貯金額を出す
+			banksDAO bnkDAO = new banksDAO();
+			int banks = bnkDAO.select(userid);
+			System.out.println(banks);
+		// 目標貯金額を出す
+			goalsDAO gDAO = new goalsDAO();
+			int goals = gDAO.select(userid);
+			System.out.println(goals);
 
 
-
+			// データを格納する
+						request.setAttribute("wnt",
+						new CalendarDate(gwant,glimit,charpic,banks,goals ));
 		    // フォワード
 		    RequestDispatcher dispatcher =
 		    	request.getRequestDispatcher
