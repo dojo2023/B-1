bea83a41879744b404f8aa10f4a93b082576069f
dojo2023-oguo <130330870+dojo2023-oguo@users.noreply.github.com>
2023-06-26 14:24:48 +0900

    DAOset

--- src/src/dao/goalsDAO.java
+++ src/src/dao/goalsDAO.java
@@ -499,4 +499,50 @@ public Timestamp getupdateat(String userid) {
 			// 結果を返す
 			return create;
 		}
-}
+
+		//目標達成画面で今までの目標をすべて削除。成功したらtrueを返す
+			public boolean delete(String userid) {
+				Connection conn = null;
+				boolean result = false;
+
+				try {
+					// JDBCドライバを読み込む
+					Class.forName("org.h2.Driver");
+
+					// データベースに接続する
+					conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/MYBC", "sa", "");
+
+					// SQL文を準備する
+					String sql = "delete from goals where user_id=?";
+					PreparedStatement pStmt = conn.prepareStatement(sql);
+
+					// SQL文を完成させる
+					pStmt.setString(1, userid);
+
+					// SQL文を実行する
+					if (pStmt.executeUpdate() == 1) {
+						result = true;
+					}
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+				catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+				finally {
+					// データベースを切断
+					if (conn != null) {
+						try {
+							conn.close();
+						}
+						catch (SQLException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+
+				// 結果を返す
+				return result;
+			}
+		}
