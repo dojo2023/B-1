55e17cbae73aadd74daa01dad13e7f1269b93df5
dojo2023-oguo <130330870+dojo2023-oguo@users.noreply.github.com>
2023-06-26 11:28:16 +0900

    Update goalsDAO.java

--- src/src/dao/goalsDAO.java
+++ src/src/dao/goalsDAO.java
@@ -449,4 +449,54 @@ public Timestamp getupdateat(String userid) {
 			// 結果を返す
 			return hanbetu;
 		}
+		//目標設定日を取得
+		public Timestamp getCreate(String userid) {
+			Connection conn = null;
+
+			Timestamp create;
+
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("org.h2.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/B1", "sa", "");
+
+				// SQL文を準備する
+				String sql = "select created_at from goals where user_id=?";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる(servletから取得したuserid(11.のString userid)をSQLに入れる)
+				pStmt.setString(1, userid);
+				System.out.println("dao29");
+
+				// SQL文を実行し、結果表を取得する
+				ResultSet rs = pStmt.executeQuery();
+				System.out.println("dao33");
+
+				// 結果表をコレクションにコピーする
+				rs.next();
+				create = rs.getTimestamp("created_at");
+			} catch (SQLException e) {
+				e.printStackTrace();
+				create = null;
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				create = null;
+				System.out.println("dao43");
+			} finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+						create = null;
+					}
+				}
+			}
+			System.out.println("dao55");
+			// 結果を返す
+			return create;
+		}
 }
