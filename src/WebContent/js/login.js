3660fe7f6ccdc3a0d3199571f0e6819075da2645
dojo2023-nakamura <nakamuramaikoplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-28 13:37:06 +0900

    ログインCSS

---
+++ src/WebContent/js/login.js
@@ -0,0 +1,6 @@
+$("#particle").particleText({
+text: "かけいぼっち", // 表示させたいテキスト。改行の場合は<br>追加
+colors:["#fff","#ccc", "#ddd"], // パーティクルの色を複数指定可能
+speed: "high", // slow, middle, high の3つから粒子が集まる速さを選択
+});
+
